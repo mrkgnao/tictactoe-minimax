9397bbf55e7a5cad90244d49d8acf09eff9c3660
Soham Chowdhury <chow.soham@gmail.com>
2015-10-17 01:50:52 +0530

    Add documentation in places

--- Board.hs
+++ Board.hs
@@ -12,40 +12,46 @@ instance Show Board where
     where lst = map show cs
           [topRow,midRow,botRow] = chunks 3 lst
 
-chunks :: Int -> [a] -> [[a]]
+-- | Found this on SO. Isn't it beautiful?
+chunks :: Index -> [a] -> [[a]]
 chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
 
+-- | The "zero board"
 emptyBoard :: Board
 emptyBoard = Board (replicate 9 E)
 
+-- | Convenience function that turns Xs into Os and vice versa.
 invert :: Board -> Board
 invert (Board cs) = Board $ map invert' cs
   where invert' X = O
         invert' O = X
         invert' E = E
 
--- FIXME use Maybe?
-getCell :: Int -> Board -> Cell
+-- | Not using Maybe because this will not be exposed.
+getCell :: Index -> Board -> Cell
 getCell ix (Board cs)
   | 0 <= ix && ix <= 8 = cs !! ix
   | otherwise = error $ "Invalid index " ++ show ix
 
-setCell :: Int -> Cell -> Board -> Board
+-- | Set a cell of a board to a particular value.
+-- | FIXME fail if user attempts to update non-empty cell?
+setCell :: Index -> Cell -> Board -> Board
 setCell ix cell (Board cs) =
   Board $ cs // (ix,cell)
   where ls // (n,item) = a ++ (item : b)
           where (a,_:b) = splitAt n ls
 
-rows :: Board -> [[Cell]]
+-- | Functions for working with the three kinds of subsets of a board
+-- | that a player can "win": rows, columns and diagonals.
+rows, cols, diags :: Board -> [[Cell]]
 rows (Board cs) = chunks 3 cs
-
-cols :: Board -> [[Cell]]
-cols (Board cs) = map (col cs) [0,1,2]
-  where
-    col :: [Cell] -> Int -> [Cell]
-    col cs ix = map (\n -> cs !! (ix + 3 * n)) [0,1,2]
-
-diags :: Board -> [[Cell]]
-diags (Board cs) = [diag, antidiag]
+cols (Board cs) =
+  map (col cs)
+      [0,1,2]
+  where col :: [Cell] -> Index -> [Cell]
+        col cs ix =
+          map (\n -> cs !! (ix + 3 * n))
+              [0,1,2]
+diags (Board cs) = [diag,antidiag]
   where diag = map (cs !!) [0,4,8]
         antidiag = map (cs !!) [2,4,7]
--- Player.hs
+++ Player.hs
@@ -11,11 +11,16 @@ toCell PO = O
 move :: Player -> Int -> Board -> Board
 move player pos = setCell pos (toCell player)
 
+-- | possibleBoards returns all board configurations that the current player can
+-- | reach within one move.
 possibleBoards :: Player -> Board -> [Board]
 possibleBoards player board = map (\pos -> move player pos board) posns
   where posns = filter (\ix -> getCell ix board == E) [0..8]
 
-hasWon :: Player -> Board -> Bool
+-- | hasWon and hasLost will be very important for evaluating nodes of the game
+-- | tree later. A player has won if at least one of the three rows, three
+-- | and two diagonals "belongs" to them.
+hasWon, hasLost :: Player -> Board -> Bool
 hasWon player board = or conditions
   where conditions :: [Bool]
         conditions =
@@ -24,3 +29,5 @@ hasWon player board = or conditions
                            (f board))
                     [rows,cols,diags]
         c = toCell player
+-- Hehehe
+hasLost player board = hasWon player (invert board)
